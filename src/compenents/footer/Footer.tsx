c0aa06b76159876c2370eb07ced1ee9066d44855
Sergey Vikhristyuk <sergey_vikhristyuk@epam.com>
2024-03-07 17:31:06 +0500

    Use wrapper for footer

--- src/compenents/footer/Footer.tsx
+++ src/compenents/footer/Footer.tsx
@@ -13,66 +13,75 @@ import tg from "@/assets/logos/ic_sharp-telegram.svg";
 import imgTg from "@/assets/logos/logos_telegram.svg";
 import wat from "@/assets/logos/logos_whatsapp-icon.svg";
 import mail from "@/assets/logos/mdi_email-box.svg";
+import Wrapper from "../wrapper/Wrapper";
 const Footer: FC = () => {
   return (
     <footer className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={styles.links}>
-            <h4>Разделы</h4>
-            <div className={styles.items}>
-              {footerLinksSec.map((item, i) => (
-                <Link href={item.link} key={i}>
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className={styles.links}>
-            <h4>Покупателям</h4>
-            <div className={styles.items}>
-              {footerLinksBuyer.map((item, i) => (
-                <Link href={item.link} key={i}>
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className={styles.links}>
-            <h4>Партнерам</h4>
-            <div className={styles.items}>
-              {footerLinksPart.map((item, i) => (
-                <Link href={item.link} key={i}>
-                  {item.name}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div>
-            <div className={styles.links}>
-              <h4>Соц сети</h4>
-              <div className={`${styles.items} ${styles.social}`}>
-                <img src={vk.src} alt="" />
-                <img src={tg.src} alt="" />
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <Wrapper>
+              <div className={styles.rowInner}>
+                <div className={styles.links}>
+                  <h4>Разделы</h4>
+                  <div className={styles.items}>
+                    {footerLinksSec.map((item, i) => (
+                      <Link href={item.link} key={i}>
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+                <div className={styles.links}>
+                  <h4>Покупателям</h4>
+                  <div className={styles.items}>
+                    {footerLinksBuyer.map((item, i) => (
+                      <Link href={item.link} key={i}>
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+                <div className={styles.links}>
+                  <h4>Партнерам</h4>
+                  <div className={styles.items}>
+                    {footerLinksPart.map((item, i) => (
+                      <Link href={item.link} key={i}>
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+                <div>
+                  <div className={styles.links}>
+                    <h4>Соц сети</h4>
+                    <div className={`${styles.items} ${styles.social}`}>
+                      <img src={vk.src} alt="" />
+                      <img src={tg.src} alt="" />
+                    </div>
+                  </div>
+                  <div className={styles.links}>
+                    <h4>Связаться с нами</h4>
+                    <div className={`${styles.items} ${styles.social}`}>
+                      <img src={imgTg.src} alt="" />
+                      <img src={wat.src} alt="" />
+                      <img src={mail.src} alt="" />
+                    </div>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className={styles.links}>
-              <h4>Связаться с нами</h4>
-              <div className={`${styles.items} ${styles.social}`}>
-                <img src={imgTg.src} alt="" />
-                <img src={wat.src} alt="" />
-                <img src={mail.src} alt="" />
+            </Wrapper>
+          </div>
+          <div className={styles.row__two}>
+            <Wrapper>
+              <div className={styles.row__two__inner}>
+                <div className={styles.logo}>
+                  <Image src={logo} alt="" height={24} />
+                </div>
+                <p>© 2024 ЯХТЫ</p>
               </div>
-            </div>
+            </Wrapper>
           </div>
         </div>
-        <div className={styles.row__two}>
-          <div className={styles.logo}>
-            <Image src={logo} alt="" height={24} />
-          </div>
-          <p>© 2024 ЯХТЫ</p>
-        </div>
-      </div>
     </footer>
   );
 };
